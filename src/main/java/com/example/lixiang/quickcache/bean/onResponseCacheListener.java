fc914890fecd88a9fa2f412da9baf6cc1cc3a56f
lixiang <lixianglook@qq.com>
2017-04-27 21:38:32 +0800

    init-three

--- src/main/java/com/example/lixiang/quickcache/bean/onResponseCacheListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.lixiang.quickcache.bean;
-
-public abstract class onResponseCacheListener{
-       public abstract  void onResponseCache(String onResponseData);
-
-        public void onRequestNetWork() {
-            
-        }
-    }
